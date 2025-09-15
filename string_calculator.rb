1d89bfcc978df0fef30f3a54ec1d5461045be93a
Swapnil Kumawat <swapnil.kumawat@paritycube.com>
2025-09-15 18:55:55 +0530

    Support custom delimiter defined at the start of input

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,15 @@ class StringCalculator
 
   def self.add(numbers)
     return 0 if numbers.strip.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      delimiter, nums = numbers.match(%r{\/\/(.)\n(.*)}).captures
+      numbers = nums.split(delimiter)
+    else
+      numbers = numbers.split(/,|\n/)
+    end
+  
+    numbers.map(&:to_i).sum
   end
 
 end
