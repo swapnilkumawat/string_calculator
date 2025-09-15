88d17e24d46cb222ed2126675fb1bb9094f8471c
Swapnil Kumawat <swapnil.kumawat@paritycube.com>
2025-09-15 18:53:51 +0530

    Add failing test for custom delimiter defined at the start

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
 
+    it "handles custom delimiter defined at the start" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
   end
 end
