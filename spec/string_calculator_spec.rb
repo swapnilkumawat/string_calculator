0024200f4a94a037a84c55f5e77cf80c0876f0a4
Swapnil Kumawat <swapnil.kumawat@paritycube.com>
2025-09-15 18:42:51 +0530

    add failing test for single number string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,8 +2,14 @@ require_relative "../string_calculator"
 
 RSpec.describe StringCalculator do
   describe "#add" do
+
     it "returns 0 for empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number when input has a single number" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
   end
 end
