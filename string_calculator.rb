757ece4d98f8922fe22693e10c2d262720c7550d
Swapnil Kumawat <swapnil.kumawat@paritycube.com>
2025-09-15 19:04:05 +0530

    Refactor code for readability using methods

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,21 +3,33 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.strip.empty?
 
+    delimiters, numbers_part = parse_delimiter(numbers)
+    num_list = split_numbers(numbers_part, delimiters)
+    check_negatives(num_list)
+    num_list.sum
+  end
+
+  private
+
+  def self.parse_delimiter(numbers)
     if numbers.start_with?("//")
       delimiter, nums = numbers.match(%r{\/\/(.)\n(.*)}).captures
-      numbers = nums.split(delimiter)
+      [[delimiter], nums]
     else
-      numbers = numbers.split(/,|\n/)
+      [ [",", "\n"], numbers ]
     end
+  end
 
-    numbers = numbers.map(&:to_i)
+  def self.split_numbers(numbers_part, delimiters)
+    pattern = Regexp.union(delimiters)
+    numbers_part.split(pattern).map(&:to_i)
+  end
 
+  def self.check_negatives(numbers)
     negatives = numbers.select { |n| n < 0 }
     unless negatives.empty?
       raise "negative numbers not allowed #{negatives.join(',')}"
     end
-
-    numbers.sum
   end
 
 end
